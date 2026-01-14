857bd3440119ec34872d039848913367c4a413c5
yosuasetian-id <joshuasetianugraha@gmail.com>
2026-01-14 17:47:55 +0700

    Make register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => 'required|max:16',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:8|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/|confirmed'
+        ]);
+
+        $user = User::create($fields);
+
+        return response()->json([
+            'message' => 'Register successfully!',
+            'user' => $user
+        ], 201);
+    }
 }
