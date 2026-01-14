fe3647549765db31ff69d2b0410b3ddebe3b32ae
yosuasetian-id <joshuasetianugraha@gmail.com>
2026-01-14 17:56:33 +0700

    edit message

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,7 +12,9 @@ class AuthController extends Controller
         $fields = $request->validate([
             'name' => 'required|max:16',
             'email' => 'required|email|unique:users,email',
-            'password' => 'required|min:8|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/|confirmed'
+            'password' => 'required|min:8|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#]).+$/|confirmed'
+        ], [
+            'password.regex' => 'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character!'
         ]);
 
         $user = User::create($fields);
